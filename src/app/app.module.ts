8e12cf45ecd1757fdfee28bff859c4e2b662b1d5
Dom-Marra <dominicmarra321@gmail.com>
2020-12-08 10:16:01 -0800

    Added shopping list component, which will serve as the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppinglistComponent
   ],
   imports: [
     BrowserModule,
