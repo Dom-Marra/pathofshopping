d572b2a07a0ab448ebb225246fc78d79b9051956
Dom-Marra <dominicmarra321@gmail.com>
2020-12-08 14:29:08 -0800

    Added base properties of the shopping list

--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppinglistComponent implements OnInit {
 
+  private budget: number = 0;                                   //Users budget
+  private estCost: number = 0;                                  //Estimated Cost of the items
+  private budgetCurrencyType = null;                            //Type of currency to use for items
+  private shoppingListName: string = "Your Shopping List";      //Name of the shopping list
+  private league = null;                                        //League to use for indexing items
+
   constructor() { }
 
   ngOnInit(): void {
