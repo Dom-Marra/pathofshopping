fbf10e5567f16267d8f40364c5e076f57aa87b9e
Dom-Marra <dominicmarra321@gmail.com>
2020-12-12 12:38:05 -0800

    Added variable for expansion state of the component, and set encapsulation

--- src/app/shoppinglist/item/item.component.ts
+++ src/app/shoppinglist/item/item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ItemComponent implements OnInit {
+
+  private disableExpansion: boolean = false;          //Disable state of the expansion of the item
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Sets the disable state of the expansion of the item contents
+   * 
+   * @param disable
+   *        boolean 
+   */
+  public setDisableExpansion(disable: boolean) {
+    this.disableExpansion = disable;
+  }
+
+  /**
+   * Returns the disable state of the expansion of the item contents
+   * 
+   * @returns
+   *        disableExpansion: boolean 
+   */
+  public getDisableExpansion(): boolean {
+    return this.disableExpansion;
   }
 
 }
