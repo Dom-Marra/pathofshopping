d7f9f5807a09db892abc2de7c9bca1cce6c52a16
Dom-Marra <dominicmarra321@gmail.com>
2020-12-12 19:29:52 -0800

    Added base code

--- src/app/shoppinglist/item-service.service.ts
+++ src/app/shoppinglist/item-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item } from './item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemServiceService {
 
+  private items: Observable<Array<Item>> = new Observable();
+
   constructor() { }
+
+  /**
+   * Adds an item to the array of items
+   * 
+   * @param item
+   *        Item: the item to add 
+   */
+  public addItem(item: Item) {
+    this.items.subscribe(items => {
+      items.push(item);
+    })
+  }
+
+  /**
+   * Returns the observable of items
+   * 
+   * @returns
+   *        Observable<Array<Item>>: the items
+   */
+  public getItems(): Observable<Array<Item>> {
+    return this.items;
+  }
 }
