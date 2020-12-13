b2c2047852f9e253f90772a33cda1b547cba9d67
Dom-Marra <dominicmarra321@gmail.com>
2020-12-12 19:29:38 -0800

    Added base code

--- src/app/shoppinglist/item.ts
+++ src/app/shoppinglist/item.ts
@@ -1,2 +1,30 @@
 export class Item {
+
+    private name: string = 'New Item';
+
+    constructor(name: string) {
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
+    /**
+     * Sets the name of the item
+     * 
+     * @param name 
+     *        String: name of the item
+     */
+    public setName(name: string) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name of the item
+     * 
+     * @returns
+     *          string: name of the item
+     */
+    public getName(): string {
+        return this.name;
+    }
 }
