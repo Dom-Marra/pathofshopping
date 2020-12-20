959463147b04dc12f11b7f1b64b59ad31c88a9ed
Dom-Marra <dominicmarra321@gmail.com>
2020-12-19 19:44:25 -0800

    Fixed import and created http client

--- src/app/itemsearch.service.ts
+++ src/app/itemsearch.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
@@ -8,5 +8,5 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class ItemsearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
