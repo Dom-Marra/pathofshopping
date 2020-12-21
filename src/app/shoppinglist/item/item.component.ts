9c2d61721f6e8284a422145f67253d608bae6494
Dom-Marra <dominicmarra321@gmail.com>
2020-12-21 12:08:19 -0800

    Created filter functions

--- src/app/shoppinglist/item/item.component.ts
+++ src/app/shoppinglist/item/item.component.ts
@@ -1,6 +1,14 @@
 import { ChangeDetectorRef, Component, ElementRef, OnInit, QueryList, ViewChildren, ViewEncapsulation, ViewRef } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { ItemsearchService, searchItem } from '../../itemsearch.service';
+import {map, startWith} from 'rxjs/operators';
+
+export const filterSearch = (items: Array<string>, searchText: string): Array<string> => {    //Filters items by search text
+  const text = searchText.toLowerCase();
+
+  return items.filter(item => item.toLowerCase().indexOf(text) != -1);
+}
 
 @Component({
   selector: 'app-item',
@@ -12,11 +20,12 @@ export class ItemComponent implements OnInit {
 
   @ViewChildren("itemNameInput") itemNameInput: QueryList<ElementRef>;    //Item name input element
 
-  private editName: boolean = false;                  //Whether name is in edit mode or not
+  private editName: boolean = false;                      //Whether name is in edit mode or not
 
-  private viewRef: ViewRef = null;                    //Reference of the view, used when deleting the component
+  private viewRef: ViewRef = null;                        //Reference of the view, used when deleting the component
 
-  private itemsToSearch: Array<searchItem> = [];
+  private itemsToSearch: Array<searchItem> = [];          //POE items
+  private filteredItems: Observable<Array<searchItem>>;   //Filtered results of the items
 
   public itemForm = new FormGroup({
     itemName: new FormControl('New Item'),
@@ -26,7 +35,12 @@ export class ItemComponent implements OnInit {
   });
 
   constructor(private cd: ChangeDetectorRef, private itemSearch: ItemsearchService) { 
-    this.itemsToSearch = this.itemSearch.getItems();
+    this.itemsToSearch = this.itemSearch.getItems();                            //Init items to search
+
+    this.filteredItems = this.itemForm.controls.itemSearch.valueChanges.pipe(   //filter items when item search changes
+      startWith(''),
+      map(searchText => this.filterGroups(searchText))
+    );
   }
 
   ngAfterViewInit() {
@@ -40,6 +54,35 @@ export class ItemComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Filters the items from each category of the search items
+   * 
+   * @param searchText
+   *        string: text to filter by
+   * 
+   * @returns 
+   *       Array<searchItem>: The filtered results
+   */
+  private filterGroups(searchText: string): Array<searchItem> {
+
+    if (searchText != '') {
+      return this.itemsToSearch.map(searchItem => ({category: searchItem.category, items: filterSearch(searchItem.items, searchText)}))
+      .filter(searchItem => searchItem.items.length > 0);
+    } else {
+      return [];
+    }
+  }
+
+  /**
+   * Returns filtered items
+   * 
+   * @returns
+   *        Observable<Array<searchItem>>: The filtered results of the search items
+   */
+  public getFilteredItems(): Observable<Array<searchItem>> {
+    return this.filteredItems;
   }
 
   /**
