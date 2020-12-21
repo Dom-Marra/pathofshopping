03936055cd5e32926fc2994d9f798fcd7943a9ff
Dom-Marra <dominicmarra321@gmail.com>
2020-12-21 10:48:36 -0800

    Exported searchItem interface

--- src/app/itemsearch.service.ts
+++ src/app/itemsearch.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface searchItem {
+export interface searchItem {
   category: string,
   name: string
 }
