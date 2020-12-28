7780dadaf1683ad9573b4c008d1b4df1023be9b0
Dom-Marra <dominicmarra321@gmail.com>
2020-12-28 15:58:47 -0800

    Changed control in the minmaxExtras interface to abstract control, and group to abstract control, included viewencapsulation none for styling

--- src/app/shoppinglist/minmaxinput/minmaxinput.component.ts
+++ src/app/shoppinglist/minmaxinput/minmaxinput.component.ts
@@ -1,22 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControlName, FormGroupName } from '@angular/forms';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 export interface minmaxExtras{
   label: string,
-  control: FormControlName,
+  control: AbstractControl,
   inputClass?: string
 }
 
 @Component({
   selector: 'app-minmaxinput',
   templateUrl: './minmaxinput.component.html',
-  styleUrls: ['./minmaxinput.component.scss']
+  styleUrls: ['./minmaxinput.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MinmaxinputComponent implements OnInit {
 
-  @Input() formGroup: FormGroupName;
-  @Input() inputName: string;
-  @Input() minmaxExtras: Array<minmaxExtras>;
+  @Input() group: AbstractControl;                      //form group to use
+  @Input() inputName: string;                           //name of the input
+  @Input() minmaxExtras: Array<minmaxExtras> = [];      //extra inputs
 
   constructor() { }
 
