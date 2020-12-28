1908a9e2ec75de063c02c341041d164e48702916
Dom-Marra <dominicmarra321@gmail.com>
2020-12-28 14:29:02 -0800

    Added inputs for component

--- src/app/shoppinglist/minmaxinput/minmaxinput.component.ts
+++ src/app/shoppinglist/minmaxinput/minmaxinput.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-minmaxinput',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./minmaxinput.component.scss']
 })
 export class MinmaxinputComponent implements OnInit {
+
+  @Input() formGroup: FormGroupName;
+  @Input() inputName: string;
 
   constructor() { }
 
