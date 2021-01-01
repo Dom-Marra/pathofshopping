90c6d8ca4a9cede35f8d941d7ec0884d081ef417
Dom-Marra <dominicmarra321@gmail.com>
2021-01-01 14:50:37 -0800

    Modified so that it conforms with the POE api query structure

--- src/app/shoppinglist/filters/requirements/requirements.component.ts
+++ src/app/shoppinglist/filters/requirements/requirements.component.ts
@@ -11,29 +11,32 @@ export class RequirementsComponent implements OnInit {
   @Input() itemForm: FormGroup;                                           //Main item form
   
   public requirementFilters: FormGroup = new FormGroup({                  //Requirement filters
-    level: new FormGroup({
-      min: new FormControl(''),
-      max: new FormControl('')
+    disabled: new FormControl(true),
+    filters: new FormGroup({
+      lvl: new FormGroup({
+        min: new FormControl(''),
+        max: new FormControl('')
+      }),
+      str: new FormGroup({
+        min: new FormControl(''),
+        max: new FormControl('')
+      }),
+      dex: new FormGroup({
+        min: new FormControl(''),
+        max: new FormControl('')
+      }),
+      int: new FormGroup({
+        min: new FormControl(''),
+        max: new FormControl('')
+      })
     }),
-    strength: new FormGroup({
-      min: new FormControl(''),
-      max: new FormControl('')
-    }),
-    dexterity: new FormGroup({
-      min: new FormControl(''),
-      max: new FormControl('')
-    }),
-    intelligence: new FormGroup({
-      min: new FormControl(''),
-      max: new FormControl('')
-    })
   })
 
   constructor() {
    }
 
   ngOnInit(): void {
-    this.itemForm.addControl('requirementFilters', this.requirementFilters);  //Add requirement filters to main form
+    this.itemForm.addControl('req_filters', this.requirementFilters);  //Add requirement filters to main form
   }
 
 }
