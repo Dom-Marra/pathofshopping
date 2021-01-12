c4696bfece9d986b1bb7db9661749db3c0615720
Dom-Marra <dominicmarra321@gmail.com>
2021-01-12 11:00:12 -0800

    Updated to include item name, and type

--- src/app/itemsearch.service.ts
+++ src/app/itemsearch.service.ts
@@ -3,7 +3,11 @@ import { HttpClient } from '@angular/common/http';
 
 export interface searchItem {
   category: string,
-  items: Array<string>
+  items: Array<{
+    name: string,
+    type: string,
+    text: string
+  }>
 }
 
 @Injectable({
@@ -23,11 +27,17 @@ export class ItemsearchService {
           let searchItem = this.items.find(item => item.category == entry['label']);    //Get search item if it exists
 
           if (searchItem != null) {               //push item to category if it exists
-            searchItem.items.push(item['text']);
+            searchItem.items.push({
+              name: item['name'],
+              type: item['type'],
+              text: item['text']});
           } else {                                //if not push new category with the item
             this.items.push({
               category: entry['label'],
-              items: [item['text']]
+              items: [{
+                name: item['name'],
+                type: item['type'],
+                text: item['text']}]
             });
           }
 
