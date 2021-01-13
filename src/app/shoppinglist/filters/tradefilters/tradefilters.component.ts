6072fb01c3bbdd8b5b537610bf6e5da0bf9e0dee
Dom-Marra <dominicmarra321@gmail.com>
2021-01-13 09:47:12 -0800

    Created trade filters component

---
+++ src/app/shoppinglist/filters/tradefilters/tradefilters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tradefilters',
+  templateUrl: './tradefilters.component.html',
+  styleUrls: ['./tradefilters.component.scss']
+})
+export class TradefiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
