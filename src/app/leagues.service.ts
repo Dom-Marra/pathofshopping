a38a41187978983bbec9c851796d1bf6fdd4040d
Dom-Marra <dominicmarra321@gmail.com>
2021-01-12 23:27:03 -0800

    Fetches leagues and stores them as an object

--- src/app/leagues.service.ts
+++ src/app/leagues.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface leagueData {
+  [id: string]: string,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaguesService {
 
-  constructor() { }
+  private leagues: leagueData = {};                          //Array of items to search from
+  private api = "http://localhost:4200/api/trade/data/leagues";     //API to fetch leagues
+
+  constructor(private http: HttpClient) { 
+
+    this.http.get(this.api).subscribe(data => { 
+      data['result'].forEach(league => { 
+        this.leagues[league.id] = league.text;
+      });
+    })
+  }
+
+  /**
+   * Returns the leagues fetched by the API
+   * 
+   * @returns
+   *        Array of league data
+   */
+  public getLeagues(): leagueData {
+    return this.leagues;
+  }
 }
