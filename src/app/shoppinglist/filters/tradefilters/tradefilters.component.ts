f80c95aa77c4e1d29a24a60b36bb3c864d9b5e65
Dom-Marra <dominicmarra321@gmail.com>
2021-01-13 10:15:46 -0800

    Created options enums for the trade filters and init the options

--- src/app/shoppinglist/filters/tradefilters/tradefilters.component.ts
+++ src/app/shoppinglist/filters/tradefilters/tradefilters.component.ts
@@ -1,6 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+enum buyOutOptions{
+  all = 'Chaos Orb Equivalent',
+  alt = "Orb of Alteration",
+  fusing = "Orb of Fusing",
+  alch = " Orb of Alchemy",
+  gcp = "Gemcutter's Prism",
+  exalted = "Exalted Orb",
+  chrom = "Chromatic Orb",
+  jewellers = "Jeweller's Orb",
+  chance = "Orb of Chance",
+  chisel = "Cartographer's Chisel",
+  scour = "Orb of Scouring",
+  blessed = "Blessed Orb",
+  regret = "Orb of Regret",
+  regal = "Regal Orb",
+  divine = "Divine Orb",
+  vaal = "Vaal Orb",
+}
+
+enum saleTypes {
+  any = 'Any',
+  all = 'Buyout or Fixed Price',
+  priced_with_info = 'Price With Note',
+  unpriced = 'No Price'
+}
+
+enum listedOptions {
+  all = 'Any Date',
+  '1day' = 'Up To 1 Day Ago',
+  '3day' = 'Up To 3 Days Ago',
+  '1week' = 'Up To 1 Week Ago',
+  '2week' = 'Up To 2 Weeks Ago',
+  '1month' = 'Up To 1 Month Ago',
+  '2month' = 'Up To 2 Months Ago'
+}
+
 @Component({
   selector: 'app-tradefilters',
   templateUrl: './tradefilters.component.html',
@@ -14,16 +50,16 @@ export class TradefiltersComponent implements OnInit {
     price: new FormGroup({
       min: new FormControl(),
       max: new FormControl(),
-      option: new FormControl()
+      option: new FormControl('all')
     }),
     account: new FormGroup({
       input: new FormControl()
     }),
     sale_type: new FormGroup({
-      option: new FormControl()
+      option: new FormControl('all')
     }),
     indexed: new FormGroup({
-      option: new FormControl()
+      option: new FormControl('all')
     })
   })
   
