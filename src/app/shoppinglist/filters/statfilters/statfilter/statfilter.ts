38816e7e2e79bec753efe78016dba2ec6bc2cce1
Dom-Marra <dominicmarra321@gmail.com>
2021-01-17 18:09:56 -0800

    Stores data pertaining to stat filters

---
+++ src/app/shoppinglist/filters/statfilters/statfilter/statfilter.ts
@@ -0,0 +1,20 @@
+import { FormArray, FormControl, FormGroup } from "@angular/forms";
+import { Stat } from "src/app/shoppinglist/statselect/stat/stat";
+
+export class Statfilter {
+
+    public stats: Array<Stat> = [];
+
+    public statFilters = new FormGroup({
+        type: new FormControl('and'),
+        filters: new FormArray([]),
+        value: new FormGroup({
+          min: new FormControl(),
+          max: new FormControl()
+        })
+      })
+
+    constructor(public statsFormArray: FormArray) {
+        this.statsFormArray.push(this.statFilters);
+    }
+}
