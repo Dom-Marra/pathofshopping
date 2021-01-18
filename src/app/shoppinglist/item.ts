04ca2a542653a0fc9f94911d91caf22dbea398db
Dom-Marra <dominicmarra321@gmail.com>
2021-01-17 18:12:09 -0800

    Has a new variable which stores stat filters

--- src/app/shoppinglist/item.ts
+++ src/app/shoppinglist/item.ts
@@ -1,6 +1,9 @@
 import { FormControl, FormGroup } from "@angular/forms";
+import { Statfilter } from "./filters/statfilters/statfilter/statfilter";
 
 export class Item {
+
+    public statFilters: Array<Statfilter> = [];
 
     constructor(private name: string) { }
     
