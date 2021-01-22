2bd36f92c360e013f9ce1b58b301ebaf77a32312
Dom-Marra <dominicmarra321@gmail.com>
2021-01-22 10:24:55 -0800

    Added funcitonality to display mods added together (hybrid mods with non hybrid)

--- src/app/shoppinglist/item/results/results.component.ts
+++ src/app/shoppinglist/item/results/results.component.ts
@@ -8,10 +8,12 @@ import { Resultdata } from './resultdata/resultdata';
 interface modData {
   text: string,
   name: string,
-  tier: string,
-  ranges: Array<{
-    min: number,
-    max: number
+  tiers: Array<{
+    tier: string,
+    ranges: Array<{
+      min: number,
+      max: number
+    }>
   }>,
   hash: string
 }
@@ -356,27 +358,36 @@ export class ParseModsPipe implements PipeTransform {
     if (item[modPropString] == null || item[modPropString].length == 0) return null;    //Return null if no explicits
 
     item[modPropString].forEach((mod, i) => {                                   //cycle through explicits
-      let hashIndex = item.extended.hashes?.[extendedPropName]?.[i][1]?.[0];    //Index of the mod for its extended data
+      let hashIndices = item.extended.hashes?.[extendedPropName]?.[i][1];        //Indices of the mod for its extended data
 
       let modData: modData = {                                      //Set data
         text: (mod as string).replace(/\n/, '<br>'),                //Add break when there is a new line
-        name: item.extended.mods?.[extendedPropName]?.[hashIndex]?.name,
-        tier: item.extended.mods?.[extendedPropName]?.[hashIndex]?.tier,  
-        ranges: [],
+        name: item.extended.mods?.[extendedPropName]?.[hashIndices?.[0]]?.name,
+        tiers: [],
         hash: item.extended.hashes?.[item.extended.hashes?.hasOwnProperty('delve') ? 'delve' : extendedPropName]
               ?.[i]?.[0]  //If it is a delve item use delve property
       }
 
-      if (item.extended.mods?.[extendedPropName]?.[hashIndex]?.magnitudes) {        //Check if there are magnitudes for the mod
-        item.extended.mods?.[extendedPropName]?.[hashIndex]?.magnitudes.forEach(magnitude => {    //Cycle through magnitudes
-          if (magnitude.hash == modData.hash 
-              && !(magnitude.min == 0 && magnitude.max == 0)) {    //If the magnitude hash matched the mod hash and the value isn't 0
-            modData.ranges.push({                                  //Add the magnitudes to the mod ranges
-              min: magnitude.min,
-              max: magnitude.max
+      if (hashIndices) {
+        for (let hashIndex of hashIndices) {
+          let tierData = {                                                        //Init data for tier
+            tier: item.extended.mods?.[extendedPropName]?.[hashIndex]?.tier,
+            ranges: [],
+          }
+  
+          if (item.extended.mods?.[extendedPropName]?.[hashIndex]?.magnitudes) {        //Check if there are magnitudes for the mod
+            item.extended.mods[extendedPropName][hashIndex].magnitudes.forEach(magnitude => {    //Cycle through magnitudes
+              if (magnitude.hash == modData.hash  && !(magnitude.min == 0 && magnitude.max == 0)) {   //If the magnitude hash matched the mod hash and the value isn't 0
+                tierData.ranges.push({           //Add the magnitudes to the tier data ranges
+                  min: magnitude.min,
+                  max: magnitude.max
+                });
+              }
             });
           }
-        });
+  
+          modData.tiers.push(tierData);
+        }
       }
 
       mods.push(modData);
