b9b35ca853ce4cdd3e0516ef90fa412a47ca46a2
Dom-Marra <dominicmarra321@gmail.com>
2021-01-25 17:40:34 -0800

    Added basic structure for loading data, this will be changed later, and removed stats array as thats no longer necessary

--- src/app/shoppinglist/item/itemdata/item.ts
+++ src/app/shoppinglist/item/itemdata/item.ts
@@ -1,15 +1,48 @@
-import { FormControl, FormGroup } from "@angular/forms";
-import { Statfilter } from "../../filters/statfilters/statfilter/statfilter";
+import { FormArray, FormControl, FormGroup } from "@angular/forms";
 import { Resultdata } from "../results/resultdata/resultdata";
 
 export class Item {
 
     public resultData: Resultdata = new Resultdata();   //Data pertaining to the results
-    public statFilters: Array<Statfilter> = [];
 
-    constructor(private name: string) { }
-    
     public itemForm = new FormGroup({               //Data pertaining to this item
         itemName: new FormControl('New Item')
     });
+
+    constructor(private name: string, public savedItemData?: any) { 
+        if (this.savedItemData != null) {
+            console.log(savedItemData);
+            this.loadSaveData();
+            this.savedItemData = null;
+        }
+    }
+
+    public loadSaveData() {
+        this.itemForm = new FormGroup({});
+        this.generateControls(this.savedItemData, this.itemForm);
+    }
+
+    public generateControls(object, group?: FormGroup, array?: FormArray) {
+        for (const key in object) {
+            if (typeof object[key] === 'object' && object[key]) {
+                let control = Array.isArray(object[key]) ? new FormArray([]) : new FormGroup({});
+
+                group?.addControl(key, control);
+                array?.push(control);
+                this.generateControls(object[key], Array.isArray(object[key]) ? null : (control as FormGroup),  Array.isArray(object[key]) ? (control as FormArray) : null);
+            } else {
+                group?.addControl(key, new FormControl(object[key]));
+            }
+        }
+    }
+
+    /**
+     * Resets the data for this item
+     */
+    public clear() {
+        this.resultData = new Resultdata();
+        this.itemForm.reset('');
+        this.itemForm.controls.itemName.patchValue('New Item');
+        (this.itemForm.get('queryForm.query.stats') as FormArray).clear();
+    }
 }
