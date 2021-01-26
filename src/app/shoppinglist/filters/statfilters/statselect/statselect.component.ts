53bbf855df196f9f24e61db441295aaa11f4d885
Dom-Marra <dominicmarra321@gmail.com>
2021-01-25 17:38:43 -0800

    Now longer uses stat class for storing data, all data is stored within a new formgroup class. This new class is taken as input and all the old functions performed in the old class is performed in the components class now. Also added disabled control for determining whether this fromgroup should be disabled on load or not. All html bindings have been updated accordingly

--- src/app/shoppinglist/filters/statfilters/statselect/statselect.component.ts
+++ src/app/shoppinglist/filters/statfilters/statselect/statselect.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
+import { StatForm } from 'src/app/shoppinglist/item/shared/stat-form';
 import { statCategory, StatsearchService } from '../services/statsearch.service';
-import { Stat } from './stat/stat';
 
 export const filterSearch = (items: any, searchText: string): any => {    //Filters items by search text
   const text = searchText.toLowerCase().trim().split(/\s+/);
@@ -24,14 +25,14 @@ export const filterSearch = (items: any, searchText: string): any => {    //Filt
 })
 export class StatselectComponent implements OnInit {
 
-  @Output() statRemoved: EventEmitter<Stat> = new EventEmitter<Stat>();  //Transmits event for removal of the stat
-  @Input() isWeight: boolean = false;                                    // Whether this stat is under a wieghted sum filter group
-  @Input() stat: Stat;                                                   //Stat that belongs to this selector
+  @Output() statRemoved: EventEmitter<StatForm> = new EventEmitter<StatForm>();     //Transmits event for removal of the stat
+  @Input() isWeight: boolean = false;                                               //Whether this stat is under a wieghted sum filter group
+  @Input() statGroup: StatForm;                                                     //Stat data that belongs to this selector
   
   public filteredStats: Array<statCategory>;                            //Filtered Stats
   public filteredStatOptions: statCategory['stats'][0]['option'];       //Filtered Stat Options
 
-  constructor(private statSearch: StatsearchService) { 
+  constructor(private statSearch: StatsearchService, private fb: FormBuilder) { 
     this.filteredStats = this.statSearch.getStats();                    //Init stats to search
   }
 
@@ -40,7 +41,13 @@ export class StatselectComponent implements OnInit {
 
   ngOnChanges(changes) {
     if (changes.isWeight) {                         //Toggle weighted on isWeighted change
-      this.stat?.makeWeighted(this.isWeight);
+      this.makeWeighted(this.isWeight);
+    }
+  }
+
+  ngAfterContentChecked() {
+    if (this.statGroup.controls.disabled.value && this.statGroup.enabled) {
+      this.statGroup.disable();
     }
   }
 
@@ -70,7 +77,7 @@ export class StatselectComponent implements OnInit {
    *        string: text to filter by
    */
   public filterStatOptions(searchText: string) {
-    let statOptions = this.stat.selectedStat.option.filter(stat => {
+    let statOptions = this.statGroup.controls.selectedStat.value.option.filter(stat => {
       return stat.text.toLowerCase().indexOf(searchText.toLowerCase()) != -1;
     });
 
@@ -78,26 +85,54 @@ export class StatselectComponent implements OnInit {
   }
 
   /**
-   * Sets the stat
-   */
-  public setStat(stat: statCategory['stats'][0]) {
-    this.stat.setStat(stat);
+     * Will add a weight form control if the isWeighted value is true,
+     * or remove it if false.
+     * 
+     * @param isWeighted 
+     */
+    public makeWeighted(isWeighted: boolean) {
+      if (isWeighted) {
+          (this.statGroup.get('value') as FormGroup).addControl('weight', new FormControl());
+      } else if (!isWeighted && this.statGroup.get('value.weight')) {
+          (this.statGroup.get('value') as FormGroup).removeControl('weight');
+      }
   }
 
   /**
-   * Sets the stat option
+   * Patches the stats id, and sets the selected stat.
+   * 
+   * Will reset option control in value form group if the stat is min max based,
+   * or reset the min max controls in said form group if said stat is option based
+   */
+  public setStat(stat: statCategory['stats'][0]) {
+      this.statGroup.controls.id.patchValue(stat.id);
+      this.statGroup.controls.selectedStat.patchValue(stat);
+
+      if (this.statGroup.controls.selectedStat.value.option == null) {
+          this.statGroup.get('value.option').patchValue('');     //Reset value option field and selected stat option when a minmax option is selected
+          this.statGroup.controls.selectedStatOption.reset();
+      } else {                                                   //reset all value fields when a stat is selected that has options
+          this.statGroup.get('value.option').patchValue('');
+          this.statGroup.get('value.min').patchValue('');
+          this.statGroup.get('value.max').patchValue('');
+      }
+  }
+
+  /**
+   * Sets the option for the selected stat
    * 
    * @param option 
-   *        option to set
+   *          option to set
    */
   public setStatOption(option: statCategory['stats'][0]['option'][0]) {
-    this.stat.setStatOption(option);
+    this.statGroup.controls.selectedStatOption.patchValue(option);
+    this.statGroup['controls'].value['controls'].option.patchValue(option.id);
   }
 
   /**
    * Triggers the stat removed event with the stat to remove as the payload
    */
   public deleteStatFilter() {
-    this.statRemoved.emit(this.stat);
+    this.statRemoved.emit(this.statGroup);
   }
 }
