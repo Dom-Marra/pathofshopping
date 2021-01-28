e365a7ef9fe32e5f984b1c019a3618b88d1b5609
Dom-Marra <dominicmarra321@gmail.com>
2021-01-28 15:04:24 -0800

    Removed everythig related to socket link extras

--- src/app/components/inputs/minmaxinput/minmaxinput.component.ts
+++ src/app/components/inputs/minmaxinput/minmaxinput.component.ts
@@ -1,13 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
-import { stylesTypes } from '../../../enums/styleTypes';
-
-export interface minmaxExtras{
-  label: string,
-  control: AbstractControl,
-  inputClass?: string
-  defaultValue?: any
-}
 
 export interface defaultValues {
   min: any,
@@ -22,7 +14,6 @@ export interface defaultValues {
 export class MinmaxinputComponent implements OnInit {
 
   @Input() group: AbstractControl;                      //form group to use
-  @Input() minmaxExtras: Array<minmaxExtras> = [];      //extra inputs
   @Input() defaultValues: defaultValues = {min: null, max: null};   //Default min max values
 
   constructor() { }
