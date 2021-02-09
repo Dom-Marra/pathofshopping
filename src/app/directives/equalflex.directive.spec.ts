346746e9009ff4979917f7753fdf35c7e689b46b
Dom-Marra <dominicmarra321@gmail.com>
2021-02-09 12:04:10 -0800

    Had to add placeholder constructor data so tests didnt fail

--- src/app/directives/equalflex.directive.spec.ts
+++ src/app/directives/equalflex.directive.spec.ts
@@ -2,7 +2,7 @@ import { EqualflexDirective } from './equalflex.directive';
 
 describe('EqualflexDirective', () => {
   it('should create an instance', () => {
-    const directive = new EqualflexDirective();
+    const directive = new EqualflexDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
