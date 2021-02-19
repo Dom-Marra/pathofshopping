869fe15fb0b255d89298a91dc08dcdaa01672bb4
Dom-Marra <dominicmarra321@gmail.com>
2021-02-19 12:17:43 -0800

    Completely refactored, so now it can be used with groups and outputs a selected event allowing the host compoenent to have control over the actions

--- src/app/components/inputs/filtergroupselect/filtergroupselect.component.ts
+++ src/app/components/inputs/filtergroupselect/filtergroupselect.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { isDefined } from '@angular/compiler/src/util';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { _MatAutocompleteBase } from '@angular/material/autocomplete';
+import { Observable } from 'rxjs';
+import { filter, map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-filtergroupselect',
@@ -9,31 +12,59 @@ import { _MatAutocompleteBase } from '@angular/material/autocomplete';
 })
 export class FiltergroupselectComponent implements OnInit {
 
-  @Input() inputName: string;                               //Name of the input
-  @Input() control: AbstractControl;                        //Form control of the input
-  @Input() selectEnum: any;                                 //Enum for values
-  @Input() autoCompleteClass: string = 'autocomplete-panel-300';
-  @Input() autoCompleteHost: _MatAutocompleteBase;
+  @Input() inputName: string;                                       //Name of the input
+  @Input() values: any;                                             //Values for the autocomplete
+  @Input() groupOptions: {                                          //Group options
+    groupedBy: string;                                              //The label of the group
+    groupedInto: string;                                            //Property holding grouped values
+  }
+  @Input() autoCompleteClass: string = 'autocomplete-panel-300';    //The class for the autocomplete panel 
+  @Input() autoCompleteHost: _MatAutocompleteBase;                  //The base element for the autocomplate panel
+  @Input() setValue: any = null;                                    //Init with this value
+  @Input() placeholder: string = 'Select Option';                   //Used for default placeholder text
+  @Input() filterBy: ((text: string, values: any) => any) | null = null;        //Function to filter by
+  @Input() displayBy: ((value: any) => string) | null = null;       //Function to get the display value
+  @Input() set disabled(value: boolean) {                           //Disables the input
+    if (value) {
+      this.search.disable();
+    } else {
+      this.search.enable();
+    }
+  }             
+  @Output() selected: EventEmitter<any> = new EventEmitter<any>();  //Emits the value selection
 
-  public filter: Array<any>;                                //Filtered results
+  public selectedValue: any = null;                         //The current selected value
+  public search: FormControl = new FormControl('');         //Search form control
+  public filteredSearch: Observable<Array<any>>;            //Filtered results
 
   constructor() { }
 
   ngOnInit(): void {
-    this.filter = Object.keys(this.selectEnum);     //Autofill the results
   }
 
-  /**
-   * Filters enumeration objects based on search text
-   * 
-   * @param searchText
-   *        string: search text 
-   * @param enumToFilter 
-   *        typeof enum: the enumeration to filter
-   */
-  public filterEnums(searchText: string, enumToFilter: any): Array<any> {
-    const text = searchText.toLowerCase();
+  ngAfterContentInit() {
+    if (this.setValue != null) {
+      this.selectValue(this.setValue);
+    }
 
-    return Object.keys(enumToFilter).filter(key => enumToFilter[key].toLowerCase().indexOf(text.toLocaleLowerCase()) != -1);
+    this.filteredSearch = this.search.valueChanges.pipe(
+      startWith(''),
+      filter(value => typeof value == 'string'),
+      map(value =>  this.filterBy(value, this.values))
+    );
+  }
+
+  public selectValue(value: any, emitEvent?: boolean): void {
+    this.selectedValue = value;
+    this.search.patchValue(this.displayBy ? this.displayBy(value): value, {emitEvent: false});
+    if (emitEvent) this.selected.emit(this.selectedValue);
+  }
+
+  public getPlaceholder(): string {
+    
+    if (isDefined(this.selectedValue)) return this.displayBy ? this.displayBy(this.selectedValue) : this.selectedValue;
+    if (this.inputName) return this.inputName;
+
+    return this.placeholder;
   }
 }
