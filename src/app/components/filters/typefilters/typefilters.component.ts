02bfaf9750246725edb15e8626c47692bcbdff05
Dom-Marra <dominicmarra321@gmail.com>
2021-02-20 12:55:18 -0800

    Updated to use the searchselect component for the item search autocomplete

--- src/app/components/filters/typefilters/typefilters.component.ts
+++ src/app/components/filters/typefilters/typefilters.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { poeCategorizedItems } from 'src/app/models/poeCategorizedItems';
 import { simpleData } from 'src/app/models/simpleData';
 import { PoeService } from 'src/app/services/poe.service';
@@ -16,7 +16,7 @@ export class TypefiltersComponent implements OnInit {
 
   public itemsToSearch: Array<poeCategorizedItems> = [];   //POE items
   public filteredItems: Array<poeCategorizedItems>;        //Filtered results of the items
-  public exactMatchFound: boolean;                         //Determines if the given search string has an exact item match
+  public matchedText: string = '';                         //Name of an items text if macthed with search
 
   public itemRarities: Array<simpleData> = [
     {id: '' , text: 'All'},
@@ -86,34 +86,33 @@ export class TypefiltersComponent implements OnInit {
     {id: 'currency.fossil', text: 'Fossil'},
     {id: 'currency.incubator', text: 'Incubator'}
   ]
-  
 
-  public search = new FormControl('');
+  public customItemSearch: poeCategorizedItems = {      //Custom search item
+    category: 'Custom Search',
+    items: [ { name: '', type: '', text: '' } ]
+  };
 
   constructor(private poeAPI: PoeService, public simpleDataService: SimpleDataService) {                    
     this.itemsToSearch = this.poeAPI.getItems();            //Init items to search
   }
 
   ngOnInit(): void {
-    if (this.queryForm.controls.term.value?.length > 0) {
-      this.search.patchValue(this.queryForm.controls.term.value);
-    } else {
-      if (this.queryForm.controls.name.value?.length > 0) this.search.patchValue(this.queryForm.controls.name.value, {emitEvent: false});
-      if (this.queryForm.controls.type.value?.length > 0) this.search.patchValue(this.search.value + " " + this.queryForm.controls.type.value, {emitEvent: false});
-    }
+  }
 
-    this.queryForm.valueChanges.subscribe(() => {
-      if (!(this.queryForm.controls.term.value || this.queryForm.controls.type.value || this.queryForm.controls.name.value)) this.search.reset('')
-    });
-
-    this.filteredItems = this.filterGroups(this.search.value);
+  /**
+   * Returns item text value
+   * 
+   * @param item
+   *        item to extract the text value from 
+   */
+  public displayItemBy(item: poeCategorizedItems["items"][0]): string {
+    return item.text;
   }
 
   /**
    * Processes the selected item from the item search autofill
    */
   public selectItem(item: poeCategorizedItems["items"][0]) {
-    this.search.patchValue(item.text);              //Set search
     this.setNTT(item.name, item.type, item.text);
   }
 
@@ -129,25 +128,12 @@ export class TypefiltersComponent implements OnInit {
    *        string: term for the search
    */
   public setNTT(itemName?: string, itemType?: string, itemTerm?: string) {
-
-    if (!itemName && !itemType && itemTerm) {
-      let itemCats = this.filterGroups(itemTerm);     //Filter items based on input
-
-      //Check if input exactly matches only 1 item, then set the item name and type
-      if (itemCats.length == 1 && itemCats[0].items.length == 1 && itemCats[0].items[0].text.toLocaleLowerCase() == itemTerm.toLocaleLowerCase()) {
-        itemName = itemCats[0].items[0].name;
-        itemType = itemCats[0].items[0].type;
-      } 
-    } else {
-      this.search.patchValue(itemTerm);
-    }
-
     if (itemName || itemType) itemTerm = null;
 
     //Patch values
-    this.queryForm.controls.name.patchValue(itemName ? itemName : '', {emitEvent: false});
-    this.queryForm.controls.type.patchValue(itemType ? itemType : '', {emitEvent: false});
-    this.queryForm.controls.term.patchValue(itemTerm ? itemTerm : '', {emitEvent: false});
+    this.queryForm.controls.name.patchValue(itemName ? itemName : '');
+    this.queryForm.controls.type.patchValue(itemType ? itemType : '');
+    this.queryForm.controls.term.patchValue(itemTerm ? itemTerm : '');
   }
   
   /**
@@ -159,44 +145,52 @@ export class TypefiltersComponent implements OnInit {
    * @returns 
    *       Array<poeCategorizedItems>: The filtered results
    */
-  public filterGroups(searchText: string): Array<poeCategorizedItems> {
+  public filterGroups(searchText: string, values: Array<poeCategorizedItems>): Array<poeCategorizedItems> {
 
-    if (!searchText) return this.itemsToSearch;     //Return whole list on empty search
+    let shift: boolean;                                   //Whether the custom search should be shifted
 
-    this.exactMatchFound = null;
+    this.customItemSearch.items[0].text = searchText;     //Update custom search text
 
-    return this.itemsToSearch.map(searchItem => ({category: searchItem.category, items: this.filterSearch(searchItem.items, searchText)}))
-    .filter(searchItem => searchItem.items.length > 0);
+    if (!searchText) return values;                       //search is empty return all values
+
+    const text = searchText.toLowerCase().trim().split(/\s+/);    //Format search text
+
+    let res = values.map(searchItem => ({
+      category: searchItem.category, 
+      items: searchItem.items.filter(item => {
+
+        //Exact match found or the the trimmed text is empty so the custom search will be shifted
+        if ((searchText.toLowerCase().trim() == item.text.toLocaleLowerCase() && item != this.customItemSearch.items[0]) || 
+            searchText.trim() === '' || !searchText) {
+          this.matchedText = item ? item.text : '';
+          shift = true;
+        }
+
+        return text.filter(text => {    //Filter items
+          return item.text.toLowerCase().indexOf(text) != -1;
+        }).length == text.length;
+      })
+    })).filter(searchItem => searchItem.items.length > 0);
+
+    this.shiftCustomSearch(shift, res);       //Perform shift
+
+    return res;
   }
 
   /**
-   * Filters the items with a given search string, and sets the exact match found variable based on results
+   * Will either shift or unshift the custom search object from the given array
    * 
-   * @param items 
-   *        Array of items to search
-   * @param searchText 
-   *        search string
+   * @param shift
+   *        boolean: whether to shift or unshift 
+   * @param values 
+   *        Array<poeCategorizedItem>: array to unshift from
    */
-  public filterSearch(items: poeCategorizedItems['items'], searchText: string): poeCategorizedItems['items'] {    //Filters items by search text
-    if (typeof searchText != 'string') return items;
-  
-    const text = searchText.toLowerCase().trim().split(/\s+/);
-  
-    return items.filter(item => {
-      return text.filter(text => {
-
-        if (searchText.toLowerCase().trim() == item.text.toLocaleLowerCase()) {
-          this.exactMatchFound = true;
-        } else if (!this.exactMatchFound) {
-          this.exactMatchFound = false;
-        }
-        
-        if (text.length > 0) {
-          return item.text.toLowerCase().indexOf(text) != -1;
-        }
-        
-        return false;
-      }).length == text.length;
-    });
+  public shiftCustomSearch(shift: boolean, values: Array<poeCategorizedItems>) {
+    if (values.indexOf(this.customItemSearch) == 0 && shift) {
+      values.shift();
+    } else if (values.indexOf(this.customItemSearch) == -1 && !shift) {
+      values.unshift(this.customItemSearch);
+    }
   }
+
 }
