5f2813af88d468847a902c938b7034dd88070f0e
Dom-Marra <dominicmarra321@gmail.com>
2021-02-26 16:10:42 -0800

    Added functionality to check if the form controls are diry then either mark the formgroup dirty or pristine based on the results

--- src/app/components/filters/otherfilters/otherfilters.component.ts
+++ src/app/components/filters/otherfilters/otherfilters.component.ts
@@ -18,12 +18,32 @@ export class OtherfiltersComponent implements OnInit {
     this.otherForm.controls.otherForm_disabled.valueChanges.subscribe(disabled => {    //When disbaled changes to false, and the form is still disabled then enable it
       if (!disabled && this.otherForm.disabled) this.otherForm.enable({emitEvent: false});
     });
+
+    for (const control in this.otherForm.controls) {
+      this.otherForm.controls[control].valueChanges.subscribe(() => {
+        this.checkIfDirty();
+      });
+    }
   }
 
   ngAfterContentChecked() {   //Disable the form when the disable value is true and the form is enabled
     if (this.otherForm.controls.otherForm_disabled.value && this.otherForm.enabled) {
       this.otherForm.disable({emitEvent: false, onlySelf: true});
     }
+  }
+
+  /**
+   * Checks if the any of the controls are dirty and marks the formgroup as dirty if true
+   */
+  public checkIfDirty() {
+    let dirty = false;
+
+    for (const control in this.otherForm.controls) { 
+      if (this.otherForm.controls[control].dirty) dirty = true;
+    }
+    
+    if (dirty) this.otherForm.markAsDirty();
+    else this.otherForm.markAsPristine();
   }
 
   /**
