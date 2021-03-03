3f4d623b8032e359151bc704766d6c24ee42a9e9
Dom-Marra <dominicmarra321@gmail.com>
2021-03-03 10:46:59 -0800

    Added input for item and created mod properties for enchant mods, implicit mods, and explicit mods

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { modProperties } from 'src/app/models/modProperties';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+  @Input() item: any;             //Full item object
+
+  public readonly enchantModProperties: Array<modProperties> = [{
+    modPropString: 'enchantMods',
+    extendedPropName: 'enchant',
+    specialClass: 'enchant'
+  }];
+
+  public readonly implicitModProperties: Array<modProperties> = [{
+    modPropString: 'implicitMods',
+    extendedPropName: 'implicit'
+  }];
+
+  public readonly explicitModProperties: Array<modProperties> = [
+    {
+        modPropString: 'fracturedMods',
+        extendedPropName: 'fractured'
+    },
+    {
+        modPropString: 'explicitMods',
+        extendedPropName: 'explicit'
+    },
+    {
+        modPropString: 'craftedMods',
+        extendedPropName: 'crafted',
+        specialClass: 'crafted'
+    },
+    {
+        modPropString: 'veiledMods',
+        extendedPropName: 'veiled',
+        specialClass: 'veiled'
+    },
+    {
+        modPropString: 'pseudoMods',
+        extendedPropName: 'pseudo',
+        specialClass: 'pseudo'
+    }
+  ];
 
   constructor() { }
 
