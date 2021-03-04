d6450608998a7158ca98ae8b70eab6551a1fc179
Dom-Marra <dominicmarra321@gmail.com>
2021-03-04 15:07:52 -0800

    Added tests

--- src/app/components/item/sortarrow/sortarrow.component.spec.ts
+++ src/app/components/item/sortarrow/sortarrow.component.spec.ts
@@ -1,4 +1,8 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconHarness } from '@angular/material/icon/testing';
 
 import { SortarrowComponent } from './sortarrow.component';
 
@@ -8,7 +12,8 @@ describe('SortarrowComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SortarrowComponent ]
+      declarations: [ SortarrowComponent ],
+      imports: [ MatIconModule ]
     })
     .compileComponents();
   });
@@ -19,7 +24,33 @@ describe('SortarrowComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Component HTML', () => {
+    let loader: HarnessLoader;
+
+    beforeEach(() => {
+      loader = TestbedHarnessEnvironment.loader(fixture);
+    });
+
+    it('should not create the icon if a value is not present', async () => {
+      component.sortValue = null;
+      await expectAsync(loader.getHarness(MatIconHarness)).toBeRejected();
+    });
+
+    it('should have the sortValue as the class on the icon', async () => {
+      component.sortValue = 'asc';
+      let icon = await loader.getHarness(MatIconHarness);
+      let host = await icon.host();
+      expect(await host.hasClass('asc')).toBeTrue();
+    });
+
+    it('should have the \'arrow_downward\' icon if the sortValue = \'desc\'', async () => {
+      component.sortValue = 'desc';
+      await expectAsync(loader.getHarness(MatIconHarness.with({name: 'arrow_downward'}))).toBeResolved();
+    });
+
+    it('should have the \'arrow_upward\' icon if the sortValue = \'asc\'', async () => {
+      component.sortValue = 'asc';
+      await expectAsync(loader.getHarness(MatIconHarness.with({name: 'arrow_upward'}))).toBeResolved();
+    });    
   });
 });
