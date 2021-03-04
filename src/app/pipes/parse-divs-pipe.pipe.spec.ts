6cc9fb15679225efa07ab4a9a11f077beb084a75
Dom-Marra <dominicmarra321@gmail.com>
2021-03-03 17:21:05 -0800

    Added tests

--- src/app/pipes/parse-divs-pipe.pipe.spec.ts
+++ src/app/pipes/parse-divs-pipe.pipe.spec.ts
@@ -1,8 +1,44 @@
+import { parsedDivData } from '../models/parsedDivData';
 import { ParseDivsPipePipe } from './parse-divs-pipe.pipe';
 
 describe('ParseDivsPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ParseDivsPipePipe();
-    expect(pipe).toBeTruthy();
+  const mods = [
+    "<uniqueitem>{Daresso's Salute}",
+    "<size:31>{<magicitem>{Bloodthirsty Eternal Sword}\r\n<default>{Item Level:} <normal>{66}}"
+  ]
+  
+  const pipe = new ParseDivsPipePipe();
+
+  it('returns expected data back', () => {
+    const expectedData: Array<parsedDivData> = [
+      {
+        values: [{
+          text: 'Daresso\'s Salute',
+          display: 'uniqueitem'
+        }]
+      },
+      {
+        values: [
+          {
+            text: "Bloodthirsty Eternal Sword",
+            display: "magicitem"
+          }
+        ]
+      },
+      {
+        values: [
+          {
+            text: "Item Level:",
+            display: "default"
+          },
+          {
+            text: ' 66',
+            display: "normal"
+          }
+        ]
+      }
+    ];
+
+    expect(pipe.transform(mods)).toEqual(expectedData);
   });
 });
